463642b40ad1e963b44acd8e3561b1fb7008b193
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-08-19 14:58:31 -0600

    reportGenerator function completed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Future tasks:
+//   Build this into a web page.
+
 // All valid credit card numbers
 const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
 const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
@@ -19,13 +22,17 @@ const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
 const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 
+// Invalid credit card number with Invalid first digit (company identifier)
+const badCompany1 = [7, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5, badCompany1]
 
 
 // Add your functions below:
 
-// validateCred function checks to see if if a given array of numbers is a valid credit card number
+// validateCred function takes in an array (credit card number)
+// checks to see if if a given credit card number is a valid credit card number
 // Returns true or false.
 const validateCred = (arr) => {
   let digitsSum = 0
@@ -53,11 +60,80 @@ const findInvalidCards = (nestedArr) => {
   let invalidNumArr = []
   nestedArr.forEach(num => {
     if (!validateCred(num)) {
-      const numArr = [num]
-      invalidNumArr.push(numArr)
+      invalidNumArr.push(num)
     }
   })
   return invalidNumArr
+}
+
+// idInvalidCardCompanies function takes in a list of invalid card numbers
+// Returns an array of company names without duplicates.
+const idInvalidCardCompanies = (invalidCardNumArr) => {
+  let companiesArr = []
+  invalidCardNumArr.forEach(num => {
+    if (num[0] === 3) {
+      if (!companiesArr.includes('Amex')) {
+        companiesArr.push('Amex')
+      }
+    } else if (num[0] === 4) {
+      if (!companiesArr.includes('Visa')) {
+        companiesArr.push('Visa')
+      }
+    } else if (num[0] === 5) {
+      if (!companiesArr.includes('Mastercard')) {
+        companiesArr.push('Mastercard')
+      }
+    } else if (num[0] === 6) {
+      if (!companiesArr.includes('Discover')) {
+        companiesArr.push('Discover')
+      }
+    } else {
+      console.log(`Company not found for invalid card number: ${convertToString(num)}`)
+    }
+  })
+  return companiesArr
+}
+
+// convertToString function takes in an array of numbers.
+// Returns a string.
+const convertToString = (arr) => {
+  let strOut = ''
+  let arrString = arr.toString()
+  for (let i = 0; i < arrString.length; i++) {
+    if (arrString[i] !== ',') {
+      strOut += arrString[i]
+    }
+  }
+  return strOut
+}
+
+// reportGenerator function takes in a nested array of card numbers (batch).
+// console logs a report of valid number, invalid numbers, and invalid numbers with unknown companies.
+const reportGenerator = (batch) => {
+  let numValid = 0
+  let numInvalid = 0
+  let invalidList = []
+  let companyList = []
+  batch.forEach(num => {
+    if (validateCred(num)) {
+      numValid ++
+      console.log(`Credit card number ${convertToString(num)} is a valid number.`)
+    } else {
+      numInvalid ++
+      console.log(`Credit card number ${convertToString(num)} is not a valid number`)
+    }
+  })
+  console.log(`This batch of numbers contains ${numValid} valid card numbers and ${numInvalid} invalid card numbers.`)
+  console.log(`The invalid card numbers are listed below:`)
+  invalidList = findInvalidCards(batch)
+  invalidList.forEach(num => {
+    console.log(convertToString(num))
+  })
+  console.log('The following companies have issued invalid card numbers:')
+  companyList = idInvalidCardCompanies(invalidList)
+  companyList.forEach(company => {
+    console.log(company)
+  })
 }
 
 // Testing Section
@@ -78,7 +154,14 @@ const findInvalidCards = (nestedArr) => {
 // console.log('Argument sent to findInvalidCards [valid1, valid2, valid3, invalid1, invalid2, invalid3]')
 // console.log('Result should be [[4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5],[5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3],[3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]')
 // console.log(`Result: ${findInvalidCards([valid1,valid2,valid3,invalid1,invalid2,invalid3])}`)
+// console.log(`invalidNumArr[0]: ${invalidNumArr[0]}`)
 
+// idInvalidCardCompanies function test lines:
+// console.log('Test Array: [invalid1, invalid2, invalid3, invalid4, invalid5, [7, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]]')
+// console.log(`Result: ${idInvalidCardCompanies([invalid1, invalid2, invalid3, invalid4, invalid5, [7, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]])}`)
 
+// reportGenerator function test lines:
+reportGenerator(batch)
 
-
+// Troubleshooting tests:
+// console.log(`valid1 converted to string: ${convertToString(valid1)}`)
